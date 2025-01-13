1b455a7d093a613a4a9b4fad27feeb395c98cf2a
giteaAdmin <bhavye.sharma1996@gmail.com>
2025-01-12 18:26:05 -0800

    :Revert "changes"
    This reverts commit 35144699a46aa33654158845368c0f2df24f1adc.

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,9 +1,18 @@
 import { defineStorage } from "@aws-amplify/backend";
 
 export const storage = defineStorage({
-  name: "storage",
+  name: "storage-browser-test",
   access: (allow: any) => ({
-    '2025/{entity_id}/*': [
+    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'media-readonly/*': [allow.authenticated.to(['read'])],
+    'shared-folder-readwrite/*': [
+      allow.authenticated.to(['read', 'write'])
+    ],
+    'protected-useronlyreadwritedelete/{entity_id}/*': [
+      allow.authenticated.to(['read']),
+      allow.entity('identity').to(['read', 'write', 'delete'])
+    ],
+    'private-useronlyreadwritedelete/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
